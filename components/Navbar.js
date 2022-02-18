9f00025587bd822e452c7af76a1ca500c234f5f0
AntonBrito <tonybrito85@gmail.com>
2022-02-18 01:12:47 -0500

    Cleaned some extra code

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,32 +14,15 @@ const Nav = styled.nav`
 const StyledLink = styled.a`
     padding: 0rem 2rem;
 `
-// const Navbar = () => {
-//     return (
-//     <Nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
-//         <div className="container">
-//             <a className="navbar-brand" href="#">InterWovenCare</a>
-//             <div className="collapse navbar-collapse">
-//                 <ul className="navbar-nav ml-auto">
-//                     <li className="nav-item">
-//                         <Link href="/"><a className="nav-link">Home</a></Link>
-//                     </li>
-//                     <li className="nav-item">
-//                         <Link href="/about"><a className="nav-link">About</a></Link>
-//                     </li>
-//                 </ul>
-//             </div>
-//         </div>
-//     </Nav>
-//     )
-// }
+
+
 
 const Navbar = () => {
     return (
     <Nav>
         <div>
-            <Link href='/'>
-                <StyledLink>InterWovenCare</StyledLink>
+            <Link href='/'className="titulo">
+                <StyledLink>Interwoven Care</StyledLink>
             </Link>
         </div>
         <div>
