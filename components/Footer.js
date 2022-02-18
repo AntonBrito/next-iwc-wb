7dc80723f2a049c2fa7e2bb34e3c82749c8b896b
AntonBrito <tonybrito85@gmail.com>
2022-02-18 00:58:48 -0500

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const FooterSection = styled.div`
+background: #000;
+height: 60px;
+display: flex;
+justify-contet: center;
+align-items: center;
+`;
+
+const Footer = () => {
+  return (
+    <FooterSection>
+      <p>Interwovencare 2022 All Rights Reserved</p>
+    </FooterSection>
+  );
+};
+
+export default Footer;
