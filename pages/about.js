452bfefedd5b8e83a510ca291797e21d9708ad07
AntonBrito <tonybrito85@gmail.com>
2022-02-18 00:58:29 -0500

    Added about page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,33 @@
+import Head from 'next/head';
+import styled from 'styled-components'
 
+
+const Hero = styled.div`
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+`;
+
+const Heading = styled.h1`
+  color: #000;
+  font-size: 5rem;
+  font-weight: 900;
+`;
 
 const about = () => {
   return ( 
-    
-    <div >
-    <h1> About </h1> 
-    </div>
-  )
+ <>
+    <Head>
+      <title>Interwovencare</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <Hero>
+      <Heading>Coming Soon</Heading>
+    </Hero>
+</>
+  );
 }
 
 export default about
