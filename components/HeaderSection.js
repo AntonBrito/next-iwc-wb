ea7145fe78208d0bf2f192a4863821dfe49d33e1
AntonBrito <tonybrito85@gmail.com>
2022-05-20 16:27:49 -0400

    Restructerd header component

---
+++ components/HeaderSection.js
@@ -0,0 +1,25 @@
+import react from 'react';
+
+
+export default function HeaderSection(props) {
+    return (
+       
+            // Sen poi flex e ta fika tudo na centro
+            // Trokal pa cor d tras-preto
+            <div className="flex bg-gray-200 py-20 px-10 flex-col items-center ">
+                <h1 className="text-5xl  text-gray-600 font-bold">
+                Interwovencare  
+                </h1>
+               <br></br>
+                <p className="text-3xl text-gray-500 fonte-bold">
+                    Full Spectrum Doula<span className="text-purple-700">&</span> Perinatal Massage Therapist
+                </p>
+                <br></br>
+                <a href="#Services" className=" bg-orange-300 rounded-md mt-8 py-3 px-4 text-gray-800 text-2xl hover:bg-orange-400">
+                    Services
+                </a>
+            </div>
+     
+       
+    );
+}
