dd5ab766397227e3349f389cff3a55f2f56c36fc
AntonBrito <tonybrito85@gmail.com>
2022-05-21 14:40:58 -0400

    Fixed the extra size of the section space

--- components/EventSection.js
+++ components/EventSection.js
@@ -8,40 +8,40 @@ export default function EventSection(props) {
        
             // Sen poi flex e ta fika tudo na centro
             // Trokal pa cor d tras-preto
-            <div className="bg-gray-800 py-20 px-10">
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 flex flex-col max-w-6xl md-flex-row">
-                    <div className="md:w-[60%] grow p-3">
-                        <h1 className="text-5xl text-gray-200 font-bold mb-10">
-                        On Site Wellness Services
-                        </h1>
-                       
-                        <p className="text-3xl text-gray-200 font-bold">
-                             
-                        </p>
-                    </div>
-                 <div className=' p-3'>  
-                     
-                         <ul href="#" className="text-gray-400 text-2xl font-bold">Relax with Massage Hydrotherapy</ul>
-                         <br></br>
-                        <ul href="#" className="text-gray-400 text-2xl font-bold">Bridal Showers</ul>
-                        <br></br>
-                         <ul href="#" className="text-gray-400 text-2xl font-bold">Yoga</ul>
-                        <br></br>
-                        <ul href="#" className="text-gray-400 text-2xl font-bold">Company wellness packages</ul>
-                        <br></br>
-                        <ul href="#" className="text-gray-400 text-2xl font-bold">Private Events</ul>
+        <div className="bg-gray-800 py-20 px-10">
+            <div className="grid grid-cols-3 md:grid-cols-1 lg:grid-cols-3 flex flex-col max-w-8xl md-flex-row">
+                <div className="md:w-[60%] grow p-3">
+                    <h1 className="text-5xl text-gray-200 font-bold mb-10">
+                    On Site Wellness Services
+                    </h1>
                     
-                     <br></br>
+                    <p className="text-3xl text-gray-200 font-bold">
+                            
+                    </p>
+                </div>
+                <div className=' p-3'>  
+                    <ul href="#" className="text-gray-400 text-2xl font-bold">Relax with Massage Hydrotherapy</ul>
+                    <br></br>
+                    <ul href="#" className="text-gray-400 text-2xl font-bold">Bridal Showers</ul>
+                    <br></br>
+                        <ul href="#" className="text-gray-400 text-2xl font-bold">Yoga</ul>
+                    <br></br>
+                    <ul href="#" className="text-gray-400 text-2xl font-bold">Company wellness packages</ul>
+                    <br></br>
+                    <ul href="#" className="text-gray-400 text-2xl font-bold">Private Events</ul>
+                
+                    <br></br>
                     <a href="#Services" className=" bg-orange-300 rounded-md mt-8 py-3 px-4 text-gray-800 text-2xl hover:bg-orange-400">
-                        Book Now!
+                    Book Now!
                     </a>
                 </div>
                 <div className="w-full p-4">
-                <Image src={massagebed} alt="Events anoucementes"/>
+                    <Image src={massagebed} alt="Events anoucementes"/>
                 </div>
                 
+            
             </div>
-            </div>
+        </div>
      
        
     );
