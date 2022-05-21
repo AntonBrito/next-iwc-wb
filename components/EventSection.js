b49f98dc3932901d9998ab26c6ddcb7e76f8be63
AntonBrito <tonybrito85@gmail.com>
2022-05-21 01:12:29 -0400

    Added event section

--- components/EventSection.js
+++ components/EventSection.js
@@ -9,19 +9,37 @@ export default function EventSection(props) {
             // Sen poi flex e ta fika tudo na centro
             // Trokal pa cor d tras-preto
             <div className="bg-gray-800 py-20 px-10">
-                <div className="grid grid-cols-2 flex flex-col max-w-6xl md-flex-row">
-                    <div className="md:w-[60%] grow p-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 flex flex-col max-w-6xl md-flex-row">
+                    <div className="md:w-[60%] grow p-3">
                         <h1 className="text-4xl text-gray-200 text-6xl font-bold mb-10">
-                            Events  
+                        On Site Wellness Services
                         </h1>
                        
                         <p className="text-3xl text-gray-200 font-bold">
-                             On Site Wellness Services
+                             
                         </p>
                     </div>
+                 <div>  
+                     <ul>
+                         <li href="#" className="text-gray-400 text-2xl font-bold">Relax with Massage Hydrotherapy</li>
+                         <br></br>
+                        <li href="#" className="text-gray-400 text-2xl font-bold">Bridal Showers</li>
+                        <br></br>
+                         <li href="#" className="text-gray-400 text-2xl font-bold">Yoga</li>
+                        <br></br>
+                        <li href="#" className="text-gray-400 text-2xl font-bold">Company wellness packages</li>
+                        <br></br>
+                        <li href="#" className="text-gray-400 text-2xl font-bold">Private Events</li>
+                     </ul>
+                     <br></br>
+                    <a href="#Services" className=" bg-orange-300 rounded-md mt-8 py-3 px-4 text-gray-800 text-2xl hover:bg-orange-400">
+                        Book Now!
+                    </a>
+                </div>
                 <div className="w-full p-4">
                 <Image src={massagebed} alt="Events anoucementes"/>
                 </div>
+                
             </div>
             </div>
      
