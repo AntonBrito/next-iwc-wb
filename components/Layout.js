e6c3463770ddbd96ea3162525098fd25409ace67
AntonBrito <tonybrito85@gmail.com>
2022-05-21 01:54:42 -0400

    Cleaned some extra space

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
-import Header from '../components/Header'
+
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export default function Layout({ children }) {
     return  (
         <div class="relative min-h-screen flex flex-col bg-black">
-            <Header />
             <Navbar />
+            
+            
             <div className="flex flex-col">
             <main className="flex-1">
                 {children}
