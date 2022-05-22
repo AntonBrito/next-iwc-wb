13ce1ffbdaf1ae7d0e0c56b93937e91ccb99c17a
AntonBrito <tonybrito85@gmail.com>
2022-05-22 14:13:09 -0400

    Added new layout and copy right tag

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FooterSection = styled.div`
-background: #000;
 height: 60px;
 display: flex;
 justify-contet: center;
@@ -13,15 +12,19 @@ align-items: center;
 
 const Footer = () => {
   return (
-    <FooterSection>
-      <div className="float-left flex items-center p-1 space-x-6 gap-8 h-60 w-40">
-                <div className="flex p-1">
+    <FooterSection className='bg-gray-800'>
+      <div className=''>
+
+      </div>
+      <p className="p-4 flex-col text-white text-center">Interwoven Care  &copy; 2022</p>
+      <div className="flex items-center p-1 space-x-6 gap-8 h-40 w-40">
+                {/* <div className="flex p-1">
                 <Link href='/'className="titulo">
                      <Image src={logo} />
                 </Link>
-                </div>
-            </div>
-      <p class="text-white text-center">2022 All Rights Reserved</p>
+                </div> */}
+                </div>     
+     
     </FooterSection>
   );
 };
